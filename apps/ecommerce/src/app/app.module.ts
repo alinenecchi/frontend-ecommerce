76ed67f6419500df32a91cbb41fb84b9cd5f8b85
aline-necchi <alinenecchi@gmail.com>
2022-12-04 16:06:13 -0300

    add pages product-list

--- apps/ecommerce/src/app/app.module.ts
+++ apps/ecommerce/src/app/app.module.ts
@@ -4,9 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent, HomePageComponent],
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    HomePageComponent,
+    ProductListComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
